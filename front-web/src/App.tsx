1c2455a4df82d80847922f362190969330de313f
Patrick McConor <slitherhead380@gmail.com>
2021-01-07 22:21:40 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
  
 
 function App() {
   return  (
 
-    <div>
+    <>
       
       <Navbar/>
+      <Home />
 
-
-    </div>
+    </>
     
 
   );
