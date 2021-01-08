6e97dbb8536ea2119abf9266237bcd34ae69f9b7
Patrick McConor <slitherhead380@gmail.com>
2021-01-08 17:40:34 -0300

    Search order location

--- front-web/src/Orders/types.ts
+++ front-web/src/Orders/types.ts
@@ -1,3 +1,4 @@
+import { type } from "os"
 
 export type Product = {
     id: number;
@@ -5,4 +6,11 @@ export type Product = {
     price: number;
     description: string;
     imageUri: string; 
+}
+
+export type OrderLocationdata = {
+    latitude: number;
+    longitude: number;
+    address: string;
+
 }
